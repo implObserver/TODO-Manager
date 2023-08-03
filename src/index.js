9782251e3e12d59c7ffead0aa91ff3908a5e293a
implObserver <prognozman@mail.ru>
2023-08-03 17:40:50 +0500

    feat: add persistent manager

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { start } from './controllers/commonControllers';
 import { setResizeObservers } from './controllers/foldersControllers';
 import { laodElements } from './controllers/localStorageControllers/common';
+import { RootFolder } from './models/folderModels';
 import './views/css/main.css'
 import { viewRootPathsThree } from './views/nodes/links';
 
