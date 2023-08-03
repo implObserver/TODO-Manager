0ca03f77c960568d6e3c14bde5a9010eb0ade5e2
implObserver <prognozman@mail.ru>
2023-08-03 21:54:31 +0500

    chore: add localstorage

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { laodElements } from './controllers/localStorageControllers/common';
 import { RootFolder } from './models/folderModels';
 import './views/css/main.css'
 import { viewRootPathsThree } from './views/nodes/links';
-localStorage.clear()
+
 if (localStorage.length > 0) {
     laodElements();
 } else {
